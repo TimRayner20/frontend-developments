398abfd4c1849d02c73f5cf25f87dae2b5d78fc9
Tim Rayner <tim.rayner@liftshare.com>
2023-06-20 09:48:54 +0100

    update: updated hero background svg to use tailwind

--- solar-vista-energy/src/views/home/components/HomeHero.vue
+++ solar-vista-energy/src/views/home/components/HomeHero.vue
@@ -1,12 +1,13 @@
-<script setup lang="ts">
-import ShowCaseBlob from "./ShowCaseBlob.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div
-    class="hero min-h-screen flex flex-col -mt-20 border relative text-center md:text-left"
+    class="hero min-h-screen flex flex-col -mt-20 relative text-center md:text-left"
   >
-    <img src="@/assets/images/bg-gradient.svg" class="bg-gradient" />
+    <img
+      src="@/assets/images/bg-gradient.svg"
+      class="bg-gradient absolute top-[-5px] right-[-100px] md:top-[-40px] md:right-[-410px] w-[95%] -z-10"
+    />
     <div class="hero-content mt-32 p-12">
       <h1 class="text-5xl text-black">Solar Vista Energy</h1>
       <p class="text-black text-xl">Solar Energy for Everyone</p>
@@ -16,13 +17,3 @@ import ShowCaseBlob from "./ShowCaseBlob.vue";
     </div>
   </div>
 </template>
-
-<style scoped lang="scss">
-.bg-gradient {
-  position: absolute;
-  top: -40px;
-  right: -410px;
-  z-index: -1;
-  width: 95%;
-}
-</style>
