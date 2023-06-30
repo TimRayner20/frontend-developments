334405fa9bab9fec864fd8434ef451e0f4726e20
Tim Rayner <tim.rayner2020@gmail.com>
2023-07-01 00:35:01 +0100

    re-styled price calc buttons

--- solar-vista-energy/src/views/home/sections/PriceCalculator.vue
+++ solar-vista-energy/src/views/home/sections/PriceCalculator.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { computed, reactive, watch } from "vue";
 //https://www.sunsave.energy/signup - price calculator
 
 enum questionType {
@@ -14,6 +14,7 @@ type calculatorStep = {
   title: string;
   stepCompleted: boolean;
   question?: question;
+  answered: boolean;
 };
 
 type question = {
@@ -41,6 +42,7 @@ const defaultSteps: calculatorStep[] = [
       answer: "",
       type: questionType.choice,
     },
+    answered: false,
   },
 
   {
@@ -52,6 +54,7 @@ const defaultSteps: calculatorStep[] = [
       answer: "",
       type: questionType.number,
     },
+    answered: false,
   },
   {
     index: 2,
@@ -63,6 +66,7 @@ const defaultSteps: calculatorStep[] = [
       answer: "",
       type: questionType.choice,
     },
+    answered: false,
   },
   {
     index: 3,
@@ -73,37 +77,62 @@ const defaultSteps: calculatorStep[] = [
       answer: "",
       type: questionType.number,
     },
+    answered: false,
   },
   {
     index: 4,
-    title: "Where would you like the solar panels to be installed? (address)",
+    title:
+      "Where would you like the solar panels to be installed? (address, our adv satalite tech will determine roof space)",
     stepCompleted: false,
     question: {
       question: "Where would you like the solar panels to be installed?",
       answer: "",
       type: questionType.text,
     },
+    answered: false,
   },
   {
     index: 5,
-    title: "How large is your roof? (m2)",
+    title: "What's your email address?",
     stepCompleted: false,
     question: {
-      question: "How large is your roof?",
+      question: "Now we just need your email address",
       answer: "",
-      type: questionType.number,
+      type: questionType.text,
     },
+    answered: false,
   },
   {
     index: 6,
     title: "You could save (with contact form)",
     stepCompleted: false,
+    answered: false,
   },
 ];
+
 const data = reactive({
   currentCalculatorStep: defaultSteps[0] as calculatorStep,
   calculatorSteps: defaultSteps as calculatorStep[],
 });
+
+const selectChoice = (choice: string) => {
+  data.currentCalculatorStep.question!.answer = choice;
+  data.currentCalculatorStep.answered = true;
+};
+
+watch(
+  () => data.currentCalculatorStep.question?.answer,
+  () => {
+    console.log("watch");
+    if (data.currentCalculatorStep.question) {
+      const answer = data.currentCalculatorStep.question.answer;
+
+      if (typeof answer === "number" || answer.length) {
+        data.currentCalculatorStep.answered = true;
+      }
+    }
+  }
+);
 
 const incrementStep = () => {
   console.log("I worked");
@@ -139,22 +168,30 @@ const decrementStep = () => {
           }}
         </h3>
         <div class="" v-if="data.currentCalculatorStep.question">
+          <!-- Choice -->
           <div
             v-if="
             data.currentCalculatorStep.question!.type === questionType.choice 
           "
+            class="container grid grid-cols-1 md:grid-cols-2 m-auto items-center align-middle w-full text-center"
           >
             <div
-              class="mb-6 px-5"
+              class="mb-6"
               v-for="choice in data.currentCalculatorStep.question!.choices"
             >
               <button
-                class="btn bg-[#41B883] text-white rounded-md p-3 mt-6 w-1/2 mr-5 m-auto md:ml-0"
+                class="btn bg-[#c4c9c7] text-white rounded-md p-3 mt-6 w-full lg:w-3/4 mr-5 m-auto md:ml-0 m"
+                @click="selectChoice(choice)"
+                :class="{
+                  answered:
+                    data.currentCalculatorStep.question.answer === choice,
+                }"
               >
                 {{ choice }}
               </button>
             </div>
           </div>
+          <!-- Number-->
           <div
             v-if="
             data.currentCalculatorStep.question!.type === questionType.number
@@ -163,16 +200,26 @@ const decrementStep = () => {
             <input
               type="number"
               v-model="data.currentCalculatorStep.question!.answer"
+              class="shadow appearance-none border rounded w-full md:w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline m-auto"
             />
           </div>
+          <!-- Text-->
           <div
             v-if="data.currentCalculatorStep.question!.type === questionType.text"
           >
             <input
               type="text"
               v-model="data.currentCalculatorStep.question!.answer"
+              class="shadow appearance-none border rounded w-full md:w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline m-auto"
             />
           </div>
+        </div>
+        <div class="" v-else>
+          <p>With the information you have provided, you could save:</p>
+          <p>£1000.00</p>
+          <p>per year</p>
+          <p>£250000.00</p>
+          <p>over 25 years</p>
         </div>
       </div>
     </div>
@@ -182,6 +229,7 @@ const decrementStep = () => {
         v-if="data.currentCalculatorStep.index > 0"
         type="button"
         @click="decrementStep"
+        class="btn bg-[#41B883] text-white rounded-md p-3 mt-6 mr-5 m-auto md:ml-0"
       >
         Back
       </button>
@@ -191,9 +239,21 @@ const decrementStep = () => {
         "
         type="button"
         @click="incrementStep"
+        :disabled="!data.currentCalculatorStep.answered"
+        class="btn text-white rounded-md p-3 mt-6 mr-5 m-auto md:ml-0"
+        :class="{
+          'bg-[#41B883]': data.currentCalculatorStep.answered,
+          'bg-[#c4c9c7]': !data.currentCalculatorStep.answered,
+        }"
       >
         Next
       </button>
     </div>
   </div>
 </template>
+<style scoped lang="scss">
+.answered {
+  background-color: #41b883;
+  color: white;
+}
+</style>
