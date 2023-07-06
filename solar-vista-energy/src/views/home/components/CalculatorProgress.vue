e2ec30c0c9f6297ebb52940c411a84ba2d9e7227
Tim Rayner <tim.rayner@liftshare.com>
2023-07-06 15:39:10 +0100

    update: updated price calculator progress bar to use tailwind css where relevant

--- solar-vista-energy/src/views/home/components/CalculatorProgress.vue
+++ solar-vista-energy/src/views/home/components/CalculatorProgress.vue
@@ -48,10 +48,10 @@ const cssStyle = computed(() => {
 </script>
 
 <template>
-  <div class="steps-container" :style="cssStyle">
-    <ul class="steps-list">
+  <div class="steps-container w-[95%] m-auto" :style="cssStyle">
+    <ul class="steps-list flex list-none">
       <li
-        class="step"
+        class="step flex items-center flex-grow-[1] max-w-[100%] relative h-[60px]"
         v-for="(step, index) in data.steps"
         :key="index"
         :class="{
@@ -59,9 +59,15 @@ const cssStyle = computed(() => {
           'step-done': index < data.currentStep,
         }"
       >
-        <div class="step-bubble"></div>
-        <div class="step-line">
-          <div class="line-fill"></div>
+        <div
+          class="step-bubble w-[35px] h-[35px] rounded-full bg-[--passive-color] transition-all duration-[0.3s] ease-in-out flex items-center justify-center absolute"
+        ></div>
+        <div
+          class="step-line w-full h-[5px] bg-[--passive-color] top-[50%] left-0 -z-10 transform -translate-y-1/2"
+        >
+          <div
+            class="line-fill w-0 h-[5px] bg-[--active-color] transition-all duration-[0.3s] ease-in-out"
+          ></div>
         </div>
       </li>
     </ul>
@@ -69,57 +75,9 @@ const cssStyle = computed(() => {
 </template>
 
 <style scoped>
-.steps-container {
-  width: 95%;
-  margin: 0 auto;
-}
-
-.steps-list {
-  display: flex;
-  list-style: none;
-}
-
-.step {
-  display: flex;
-  align-items: center;
-  flex-grow: 1;
-  max-width: 100%;
-  position: relative;
-  height: 60px; /* not responsive */
-}
-
 .step:last-child {
   width: 60px;
   max-width: 60px;
-}
-
-.step-bubble {
-  width: 35px;
-  height: 35px;
-  border-radius: 50%;
-  background-color: var(--passive-color);
-  transition: all 0.3s ease;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-}
-
-.step-line {
-  width: 100%;
-  height: 5px;
-  background-color: var(--passive-color);
-  top: 50%;
-  left: 0;
-  transform: translateY(-50%);
-  z-index: -10;
-}
-
-.line-fill {
-  width: 0;
-  height: 5px;
-  background-color: var(--active-color);
-  transition: all 0.3s ease;
 }
 
 .step:last-child .step-line {
