7f519febd2f14002609f688c5cf7ee34dca0be7a
Tim Rayner <tim.rayner@liftshare.com>
2023-08-01 15:06:44 +0100

    update: implemented 'yup' to provide validation rules

--- vee-validate/vee-validate-experiment/src/App.vue
+++ vee-validate/vee-validate-experiment/src/App.vue
@@ -1,32 +1,32 @@
 <script setup lang="ts">
 import { Form, Field, ErrorMessage } from "vee-validate";
+import * as yup from "yup";
+
+//rules
+const passwordRules = yup.string().required().min(8);
+const emailRules = yup.string().required().email();
 
 const onSubmit = (values: any) => {
   console.log(values);
-};
-
-const validateEmail = (value: any) => {
-  // if the field is empty
-  if (!value) {
-    return "This field is required";
-  }
-  // if the field is not a valid email
-  const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-  if (!regex.test(value)) {
-    return "This field must be a valid email";
-  }
-  // All is good
-  return true;
 };
 </script>
 
 <template>
   <div id="app">
     <Form @submit="onSubmit">
-      <Field type="email" name="email" :rules="validateEmail" />
+      <label for="email"> email </label>
+      <br />
+      <Field type="email" name="email" :rules="emailRules" />
+      <br />
       <ErrorMessage name="email" />
-
-      <button>Sign up for newsletter</button>
+      <br />
+      <label for="password"> password </label>
+      <br />
+      <Field type="password" name="password" :rules="passwordRules" />
+      <br />
+      <ErrorMessage name="password" />
+      <br />
+      <button>Log in</button>
     </Form>
   </div>
 </template>
